da921dd6e7d5572b901f0912a2b73c9751d3d593
iamgamja <hmmthinking@naver.com>
2023-05-21 08:55:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+from collections import deque
+import pygame, time
 
-import pygame
 pygame.init()
 
 screen = pygame.display.set_mode([500, 500])
 
-s = [100, 200, 300, 400, 500]
+# 왼쪽 노트들
+notes1 = deque([1, 3, 5, 7, 9])
+now1 = deque()
+# 오른쪽 노트들
+notes2 = deque([2, 4, 6, 8, 10])
+now2 = deque()
+
+start_time = time.time()
 
 while 1:
     for event in pygame.event.get():
@@ -13,17 +21,29 @@ while 1:
             break
 
     screen.fill((255, 192, 203))
+    ### 여기에 배경 추가
 
-    for c in s:
-        pygame.draw.circle(screen, (255, 255, 255), (166, c), 10)
-        pygame.draw.circle(screen, (255, 255, 255), (166*2, c), 10)
+    
+    
+    # 현재까지 지난 시간
+    diff_time = time.time() - start_time
+    
+    # 현재 시간 ~ 2초 뒤 까지 보여주기
+    while notes1 and notes1[0] <= diff_time+2:
+        now1.append(notes1.popleft())
+    while now1 and now1[0] <= diff_time:
+        now1.popleft()
+    
+    while notes2 and notes2[0] <= diff_time+2:
+        now2.append(notes2.popleft())
+    while now2 and now2[0] <= diff_time:
+        now2.popleft()
+    
+    for note in now1:
+        height = (2 - (note - diff_time)) * 250
+        pygame.draw.circle(screen, (255, 255, 255), (166, height), 10)
+    for note in now2:
+        height = (2 - (note - diff_time)) * 250
+        pygame.draw.circle(screen, (255, 255, 255), (166*2, height), 10)
 
     pygame.display.flip()
-
-    for i in range(len(s)):
-        s[i] += 0.05
-        if s[i] > 500:
-            del s[i]
-            s.append(0)
-        
-
